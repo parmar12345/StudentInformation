31c65228c5bba797b28e0a7fc4523368d53a7219
01-folder <umang.parnar.it20@gmail.comemail@gmail.com>
2026-02-17 10:03:25 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApi.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string StudentName { get; set; }
+
+        [Required]
+        public string StudentGender { get; set; }
+
+        [Range(5, 100)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Standard { get; set; }
+
+        public string FatherName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using StudentApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services FIRST
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure middleware AFTER build
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
